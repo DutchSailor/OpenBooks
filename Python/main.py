63d48526d8ed754bcb3e700ae5cab8f6237bdea2
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2023-04-29 15:18:16 +0200

    Wattjes deel 2, VB, GTB

---
+++ Python/main.py
@@ -0,0 +1,17 @@
+import glob, sys, fitz
+
+# To get better resolution
+zoom_x = 2.0  # horizontal zoom
+zoom_y = 2.0  # vertical zoom
+mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
+
+filename = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 2 Vrijstaande schoorsteenen haarden en smidsen/Constructie van gebouwen Prof. J.G. Wattjes Deel 2.pdf'
+
+path = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 2 Vrijstaande schoorsteenen haarden en smidsen/Constructie van gebouwen Prof. J.G. Wattjes Deel 2'
+
+doc = fitz.open(filename)  # open document
+i = 1
+for page in doc:  # iterate through the pages
+    pix = page.get_pixmap(matrix=mat)  # render page to an image
+    pix.save(path + '-' + str(i) + '.png')  # store image as a PNG
+    i = i + 1

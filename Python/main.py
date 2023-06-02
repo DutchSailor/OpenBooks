2828291819ebc30d01c743e3b5b40c7ce1a13cd4
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2023-06-02 07:33:30 +0200

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,12 +5,12 @@ zoom_x = 2.0  # horizontal zoom
 zoom_y = 2.0  # vertical zoom
 mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
 
-filename = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 4 Ramen Deuren Kozijnen/20230515094428609.pdf'
+filename = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 5 Binnen en buitentimmeringen leidingen/1.pdf'
 
-path = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 4 Ramen Deuren Kozijnen/Prof. J.G. Wattjes Deel 4 Ramen Deuren Kozijnen'
+path = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 5 Binnen en buitentimmeringen leidingen/Prof. J.G. Wattjes Deel 5 Binnen en buitentimmeringen leidingen-'
 
 doc = fitz.open(filename)  # open document
-i = 144
+i = 1
 for page in doc:  # iterate through the pages
     pix = page.get_pixmap(matrix=mat)  # render page to an image
     pix.save(path + '-' + str(i) + '.png')  # store image as a PNG

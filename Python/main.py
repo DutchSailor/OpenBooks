ef018a0a1eceeaad17cedc36245290842ba3e0e5
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2023-06-02 07:41:28 +0200

    Prof. J.G. Wattjes Deel 5

--- Python/main.py
+++ Python/main.py
@@ -5,12 +5,12 @@ zoom_x = 2.0  # horizontal zoom
 zoom_y = 2.0  # vertical zoom
 mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
 
-filename = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Bernoullis Vademecum/20230526164358626.pdf'
+filename = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 5 Binnen en buitenbetimmeringen leidingen/3.pdf'
 
-path = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Bernoullis Vademecum/Bernoullis Vademecum'
+path = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 5 Binnen en buitenbetimmeringen leidingen/Prof. J.G. Wattjes Deel 5 Binnen en buitenbetimmeringen leidingen-'
 
 doc = fitz.open(filename)  # open document
-i = 252
+i = 3
 for page in doc:  # iterate through the pages
     pix = page.get_pixmap(matrix=mat)  # render page to an image
     pix.save(path + '-' + str(i) + '.png')  # store image as a PNG

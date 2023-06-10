7aa29f9a5ed7216e0fcab5befa635c477e8b91e9
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2023-06-10 21:44:20 +0200

    Paneeldeuren

--- Python/main.py
+++ Python/main.py
@@ -1,17 +1,28 @@
 import glob, sys, fitz
+from glob import glob
 
+from os import path
+
+def find_ext(dr, ext):
+    return glob(path.join(dr,"*.{}".format(ext)))
+
+folder = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Paneeldeuren/'
+Prefix = "Paneeldeuren-"
+path2 = folder + Prefix
+
+pdffiles = find_ext(folder,'pdf')
+
+print(pdffiles)
+#sys.exit()
 # To get better resolution
 zoom_x = 2.0  # horizontal zoom
 zoom_y = 2.0  # vertical zoom
 mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
 
-filename = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 5 Binnen en buitenbetimmeringen leidingen/3.pdf'
-
-path = 'C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 5 Binnen en buitenbetimmeringen leidingen/Prof. J.G. Wattjes Deel 5 Binnen en buitenbetimmeringen leidingen-'
-
-doc = fitz.open(filename)  # open document
-i = 3
-for page in doc:  # iterate through the pages
-    pix = page.get_pixmap(matrix=mat)  # render page to an image
-    pix.save(path + '-' + str(i) + '.png')  # store image as a PNG
-    i = i + 1
+i = 0
+for j in pdffiles:
+    doc = fitz.open(j)  # open document
+    for page in doc:  # iterate through the pages
+        pix = page.get_pixmap(matrix=mat)  # render page to an image
+        pix.save(path2 + str(i) + '.png')  # store image as a PNG
+        i = i + 1

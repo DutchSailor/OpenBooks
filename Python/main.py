dbd99257f36b999e0344d5f2c7c109c88b60844e
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2023-10-13 15:21:23 +0200

    Prof. J.G. Wattjes Deel 8 Trappen

--- Python/main.py
+++ Python/main.py
@@ -6,9 +6,9 @@ from os import path
 def find_ext(dr, ext):
     return glob(path.join(dr,"*.{}".format(ext)))
 
-folder = "C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 7 Gewelven van steen, hout, gewapend-beton en glas/"
+folder = "C:/Users/mikev/Documents/GitHub/OpenBooks/Prof. J.G. Wattjes Deel 8 Trappen van hout, ijzer, steen en gewapend beton roltrappen liften/"
 #"C:/Users/mikev/3BM Dropbox/Maarten Vroegindeweij/Domera/10_PR_bureau_standaardisatie/50_brochure/Brochure 7 Bouwnummers/"
-Prefix = "Prof. J.G. Wattjes Deel 7 Gewelven van steen, hout, gewapend-beton en glas"
+Prefix = "Prof. J.G. Wattjes Deel 8 Trappen van hout, ijzer, steen en gewapend beton roltrappen liften"
 path2 = folder + Prefix
 
 pdffiles = find_ext(folder,'pdf')
